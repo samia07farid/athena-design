b37cd86d0333cc77f53253f4fe398f8e06450027
samia07farid <samiafarid07@gmail.com>
2020-11-19 12:15:54 -0800

    responsive yet to do

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from '../Header/Header';
+import Navbar from '../Navbar/Navbar';
+import WhatWeDo from '../WhatWeDo/WhatWeDo';
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className="main-container">
+            <Navbar/>
+            <Header/>
+            <WhatWeDo/>
         </div>
     );
 };
---
+++ src/fakedata/DoInfo.js
@@ -0,0 +1,29 @@
+const DoInfo = [
+    {
+        name: 'Experience Design',
+        image: 'https://i.imgur.com/AjBk3Bf.png',
+        description: "The point of using lorem ipsum is that it has more-or-less normal.",
+        key: '1',
+    },
+    {
+        name: 'Interface Design',
+        image: 'https://i.imgur.com/izViTDW.png',
+        description: "The point of using lorem ipsum is that it has more-or-less normal.",
+        key: '2',
+    },
+    {
+        name: 'Prototyping',
+        image: 'https://i.imgur.com/ZPcMvUP.png',
+        description: "The point of using lorem ipsum is that it has more-or-less normal.",
+        key: '3',
+    },
+    {
+        name: 'Illustration',
+        image: 'https://i.imgur.com/zgRn3xJ.png',
+        description: "The point of using lorem ipsum is that it has more-or-less normal.",
+        key: '4',
+    }
+
+
+]
+export default DoInfo;
